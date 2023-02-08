2f179204ac3511ea0c7f4b5209fe20f2dd92f014
Adesipe Adeola <princeadeola234@gmail.com>
2023-02-07 17:20:14 -0800

    committed

--- app/src/main/java/com/adeolaadesipe/singlechat/model/chats.java
+++ app/src/main/java/com/adeolaadesipe/singlechat/model/chats.java
@@ -4,4 +4,31 @@ public class chats {
 
     private String number, name ,message, date, time;
 
+    public chats(String number, String name, String message, String date, String time) {
+        this.number = number;
+        this.name = name;
+        this.message = message;
+        this.date = date;
+        this.time = time;
+    }
+
+    public String getNumber() {
+        return number;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public String getTime() {
+        return time;
+    }
 }
