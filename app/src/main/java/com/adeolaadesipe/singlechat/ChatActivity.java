940524925cbf9465a7e75759b05da7a70605dd02
Adesipe Adeola <princeadeola234@gmail.com>
2023-02-07 21:47:17 -0800

    committed

--- app/src/main/java/com/adeolaadesipe/singlechat/ChatActivity.java
+++ app/src/main/java/com/adeolaadesipe/singlechat/ChatActivity.java
@@ -1,4 +1,4 @@
-package com.adeolaadesipe.singlechat.chats;
+package com.adeolaadesipe.singlechat;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
@@ -12,19 +12,25 @@ import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
 
-import com.adeolaadesipe.singlechat.MainActivity;
-import com.adeolaadesipe.singlechat.MemoryData;
-import com.adeolaadesipe.singlechat.R;
+import com.adeolaadesipe.singlechat.adapter.ChatAdapter;
+import com.adeolaadesipe.singlechat.model.Chats;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
-import com.squareup.picasso.Picasso;
+
+import java.sql.Timestamp;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.util.Locale;
 
 import de.hdodenhof.circleimageview.CircleImageView;
 
 public class ChatActivity extends AppCompatActivity {
+    List<Chats> chatsLists;
     CircleImageView chatProfilePic, chatBackBtn;
     TextView userChatNameID, userChatOnlineStatus;
     ImageView sendChatMessageBtn;
@@ -34,12 +40,14 @@ public class ChatActivity extends AppCompatActivity {
     String getUserNumber;
     String chatKey;
     DatabaseReference reference;
+    ChatAdapter chatAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chat);
         reference = FirebaseDatabase.getInstance().getReferenceFromUrl("https://singlechatapp-a1057-default-rtdb.firebaseio.com");
+        chatsLists = new ArrayList<>();
 
 
         chatProfilePic = findViewById(R.id.chatUserProfilePic);
@@ -62,24 +70,50 @@ public class ChatActivity extends AppCompatActivity {
 
         chatSectionRV.setHasFixedSize(true);
         chatSectionRV.setLayoutManager(new LinearLayoutManager(ChatActivity.this));
-        if (chatKey.isEmpty()){
-            reference.addListenerForSingleValueEvent(new ValueEventListener() {
-                @Override
-                public void onDataChange(@NonNull DataSnapshot snapshot) {
+
+        chatAdapter = new ChatAdapter(chatsLists, ChatActivity.this);
+        chatSectionRV.setAdapter(chatAdapter);
+        reference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                if (chatKey.isEmpty()) {
                     //generate chat key, be default it is 1
                     chatKey = "1";
 
-                    if (snapshot.hasChild("chat")){
-                        chatKey = String.valueOf((snapshot.child("chat").getChildrenCount() + 1));
+                    if (snapshot.hasChild("chat")) {
+                        if (snapshot.child("chat").child(chatKey).hasChild("messages")){
+                            for (DataSnapshot messageSnapshot : snapshot.child("chat").child("messages").getChildren()){
+                                if (messageSnapshot.hasChild("msg") &&  messageSnapshot.hasChild("number")){
+
+                                    String messageTimeStamps = messageSnapshot.getKey();
+                                    String getNumber = messageSnapshot.child("number").getValue(String.class);
+                                    String getMessage = messageSnapshot.child("msg").getValue(String.class);
+
+                                    if (Long.parseLong(messageTimeStamps) > Long.parseLong(MemoryData.getLastMessage(ChatActivity.this, chatKey))){
+                                        MemoryData.saveLastMessage(messageTimeStamps, chatKey, ChatActivity.this);
+
+                                        Timestamp timestamp = new Timestamp(Long.parseLong(messageTimeStamps));
+                                        Date date = new Date(timestamp.getTime());
+                                        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("dd-MM-yyyy", Locale.getDefault());
+                                        SimpleDateFormat simpleTimeFormat = new SimpleDateFormat("hh:mm aa", Locale.getDefault());
+
+                                        Chats chatList = new Chats(getNumber, getName, getMessage, simpleTimeFormat.format(date), simpleDateFormat.format(date));
+                                        chatsLists.add(chatList);
+
+                                        chatAdapter.updateChat(chatsLists);
+                                    }
+                                }
+                            }
+                        }
                     }
                 }
+            }
 
-                @Override
-                public void onCancelled(@NonNull DatabaseError error) {
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
 
-                }
-            });
-        }
+            }
+        });
 
         sendChatMessageBtn.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -87,8 +121,7 @@ public class ChatActivity extends AppCompatActivity {
                 String getChatMessage = chatMessageField.getText().toString();
 
                 String currentTimeStamp = String.valueOf(System.currentTimeMillis()).substring(0, 10); //current timestamp
-
-                MemoryData.saveLastMessage(currentTimeStamp, chatKey, ChatActivity.this);
+                //MemoryData.saveLastMessage(currentTimeStamp, chatKey, ChatActivity.this);
 
                 reference.child("chat").child(chatKey).child("user_1").setValue(getUserNumber);
                 reference.child("chat").child(chatKey).child("user_2").setValue(getNumber);
--- app/src/main/java/com/adeolaadesipe/singlechat/adapter/ChatAdapter.java
+++ app/src/main/java/com/adeolaadesipe/singlechat/adapter/ChatAdapter.java
@@ -1,4 +1,74 @@
 package com.adeolaadesipe.singlechat.adapter;
 
-public class ChatAdapter {
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.adeolaadesipe.singlechat.MemoryData;
+import com.adeolaadesipe.singlechat.R;
+import com.adeolaadesipe.singlechat.model.Chats;
+
+import java.util.List;
+
+public class ChatAdapter extends RecyclerView.Adapter<ChatAdapter.MyViewHolder> {
+
+    List<Chats> chats;
+    Context context;
+    String userNumber;
+
+    public ChatAdapter(List<Chats> chats, Context context) {
+        this.chats = chats;
+        this.context = context;
+        this.userNumber = MemoryData.getData(context);
+    }
+
+    @NonNull
+    @Override
+    public ChatAdapter.MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return new MyViewHolder(LayoutInflater.from(parent.getContext()).inflate(R.layout.chat_layout, null));
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ChatAdapter.MyViewHolder holder, int position) {
+        Chats list = chats.get(position);
+
+        if (list.getNumber().equals(userNumber)){
+            holder.myLayout.setVisibility(View.VISIBLE);
+            holder.userLayout.setVisibility(View.GONE);
+        }else{
+            holder.myLayout.setVisibility(View.GONE);
+            holder.userLayout.setVisibility(View.VISIBLE);
+        }
+    }
+
+    @Override
+    public int getItemCount() {
+        return chats.size();
+    }
+
+    public void updateChat(List<Chats> chats){
+        this.chats = chats;
+    }
+
+    public class MyViewHolder extends RecyclerView.ViewHolder {
+        LinearLayout userLayout, myLayout;
+        TextView userMessage, myMessage, userMessageTime, myMessageTime;
+
+        public MyViewHolder(@NonNull View itemView) {
+            super(itemView);
+
+            userLayout = itemView.findViewById(R.id.userLayout);
+            myLayout = itemView.findViewById(R.id.myLayout);
+            userMessage = itemView.findViewById(R.id.userMessageID);
+            myMessage = itemView.findViewById(R.id.myMessageID);
+            userMessageTime = itemView.findViewById(R.id.userMessageTimeID);
+            myMessageTime = itemView.findViewById(R.id.myMessageTimeID);
+        }
+    }
 }
